b4369c684f63754e118e46f11f85692b5804986e
Arya-Poudel <arya.poudel20@gmail.com>
2024-10-07 20:19:39 +0545

    add config for orbit (#1)
    * add config for orbit in base
    
    * move keyframe and animation to base

--- base.ts
+++ base.ts
@@ -43,6 +43,29 @@ export default {
       },
       borderColor: {
         DEFAULT: 'rgba(var(--border))'
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
+        },
+        orbit: {
+          '0%': {
+            transform: 'rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)'
+          },
+          '100%': {
+            transform: 'rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)'
+          }
+        }
+      },
+      animation: {
+        orbit: 'orbit calc(var(--duration)*1s) linear infinite',
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
       }
     },
     fontFamily: {
