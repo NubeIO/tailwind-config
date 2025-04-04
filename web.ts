916421066a870400935cf9e15bed47b7cabb1fa0
Ritesh Shakya <riteshakya037@gmail.com>
2025-04-04 12:10:09 +0400

    Update to use typography

--- web.ts
+++ web.ts
@@ -1,3 +1,4 @@
+import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 import animate from 'tailwindcss-animate';
 
@@ -22,5 +23,5 @@ export default {
       }
     }
   },
-  plugins: [animate]
+  plugins: [animate, typography]
 } satisfies Config;
